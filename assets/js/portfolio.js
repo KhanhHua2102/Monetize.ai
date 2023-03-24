b2039a12293dec819aa0e44809172b0c05067517
Quang Khanh Hua (Henry) <22928469@student.uwa.edu.au>
2023-03-24 16:33:53 +0800

    developing download as CSV file feature

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -8,13 +8,42 @@ function randomColor() {
     return color;
 }
 
+// Convert portfolioData to CSV
+function convertToCSV(data) {
+    let csv = "Stock,Date,Quantity,Bought Price,Current Price,Returns Percent,Returns Dollar,Totals";
+    data.forEach(function(row) {
+        csv += row.stocks + ',' + row.dates + ',' + row.quantities + ',' + row.bougthPrice + ',' + row.currentPrice + ',' + row.returnsPercent + ',' + row.returnsDollar + ',' + row.totals + "\r";
+    });
+    return csv;
+}
+
+// Download CSV file
+function downloadCSV(data) {
+    let csv = convertToCSV(data);
+    let blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+    let link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = 'portfolio.csv';
+    link.style.display = 'none';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+// Export CSV file when button is clicked
+function exportCSV() {
+    let exportButton = document.getElementById("export-button");
+    exportButton.addEventListener("click", function() {
+        downloadCSV(portfolioData);
+    });
+}
+
 // Get the canvas element
 let ctx = document.getElementById("portfolio-chart").getContext("2d");
 
-// Store portfolio data from table
 let portfolioData = {
     stocks: [],
-    Dates: [],
+    dates: [],
     quantities: [],
     bougthPrice: [],
     currentPrice: [],
@@ -23,20 +52,22 @@ let portfolioData = {
     totals: []
 }
 
-// Get table data from html
+// Get table data for portfolioData
 let table = document.getElementById("portfolio-table");
 let rows = table.getElementsByTagName("tr");
-let stocks = [];
-let quantities = [];
-let totals = [];
-let colors = [];
 for (let i = 1; i < rows.length; i++) {
     let cells = rows[i].getElementsByTagName("td");
-    stocks.push(cells[0].innerHTML);
-    totals.push(cells[7].innerHTML);
-    colors.push(randomColor());
+    portfolioData.stocks.push(cells[0].innerHTML);
+    portfolioData.dates.push(cells[1].innerHTML);
+    portfolioData.quantities.push(cells[2].innerHTML);
+    portfolioData.bougthPrice.push(cells[3].innerHTML);
+    portfolioData.currentPrice.push(cells[4].innerHTML);
+    portfolioData.returnsPercent.push(cells[5].innerHTML);
+    portfolioData.returnsDollar.push(cells[6].innerHTML);
+    portfolioData.totals.push(cells[7].innerHTML);
 }
 
+// Default colors for piechart
 let defaultColors = [
 	"rgb(54, 162, 235)",
 	"rgb(75, 192, 192)",
@@ -50,20 +81,17 @@ let defaultColors = [
 	"rgb(128, 0, 0)",
 ];
 
-// Create the chart data
-let data = {
-	labels: stocks,
-	datasets: [
-		{
-			label: " totals",
-			data: totals,
-			backgroundColor: defaultColors,
-		},
-	],
-};
-
 // Create the pie chart
 let myChart = new Chart(ctx, {
 	type: "pie",
-	data: data,
+	data: {
+		labels: portfolioData.stocks,
+		datasets: [
+			{
+				label: " totals",
+				data: portfolioData.totals,
+				backgroundColor: defaultColors,
+			},
+		],
+	},
 });
