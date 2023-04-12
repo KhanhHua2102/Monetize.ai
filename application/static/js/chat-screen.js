23d898f609b4874f2fb4e4d7ef009320625a4d22
Quang Khanh Hua (Henry) <22928469@student.uwa.edu.au>
2023-04-13 00:45:18 +0800

    improve code readability and modularity

--- application/static/js/chat-screen.js
+++ application/static/js/chat-screen.js
@@ -5,38 +5,65 @@ $(document).ready(function () {
     });
 
     // Send message and receive response from gpt
-    $("#submit-button").click(function (event) {
-        event.preventDefault();
-        let input = $("#chat-input").val();
-        if (input != '') {
-            $("section").append("<div class='messages user-messages'>" + input + "</div>");
-            $(".user-messages").css("visibility", "visible");
-        }
-        console.log(input);
+    $("#submit-button")
+			.click(function (event) {
+                event.preventDefault();
+                console.log("submit button clicked");
+				sendQuery();
+			});
 
-        // Loading animation
-        var animation = `
+    $("#chat-input")
+			.on("keypress", function (event) {
+                if (event.which === 13) {
+                    event.preventDefault();
+                    console.log("enter pressed");
+					sendQuery();
+				}
+            });
+});
+
+function sendQuery() {
+	console.log("sending query");
+
+	let input = $("#chat-input").val().trim();
+	$("#chat-input").val("");
+
+	if (input != "") {
+		$("section").append(
+			"<div class='messages user-messages'>" + input + "</div>"
+		);
+		$(".user-messages").css("visibility", "visible");
+	}
+	console.log(input);
+
+	// Loading animation
+	var animation = `
             <div class="loading-messages">
                 <div class="loading-dot"></div>
                 <div class="loading-dot"></div>
                 <div class="loading-dot"></div>
             </div>
             `;
-        $('section').append(animation);
+	$("section").append(animation);
 
-        // Post request to gpt
-        $.post({
-            url: "http://127.0.0.1:8080/generate",
-            data: JSON.stringify({ prompt: input }),
-            contentType: "application/json",
-            dataType: "json",
-        }).done(function (data) {
-            console.log("finished query");
-            console.log(data);
-            $(".loading-messages").remove();
-            $("section").append("<div class='messages bot-messages'>" + data.response + "</div>");
-            $(".bot-messages").css("visibility", "visible");
-        });
-    });
-});
+	// Post request to gpt
+	postRequest(input);
+}
 
+function postRequest(input) {
+	console.log("posting request");
+	$.post({
+		url: "http://127.0.0.1:8080/generate",
+		data: JSON.stringify({ prompt: input }),
+		contentType: "application/json",
+		dataType: "json",
+	}).done(function (data) {
+		console.log("finished query");
+		console.log(data);
+		$(".loading-messages").remove();
+		$("section").append(
+			"<div class='messages bot-messages'>" + data.response + "</div>"
+		);
+		$(".bot-messages").css("visibility", "visible");
+	});
+}
--- gpt.py
+++ gpt.py
@@ -9,29 +9,21 @@ openai.api_key = config.OPENAI_API_KEY
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/generate', methods=['POST'])
 def generate():
+    print("prompt received")
+    data = request.get_json()
+    prompt = data['prompt']
+    print(prompt)
+
     def openai_completion(query):
         print("Starting GPT-3\n")
-
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo", messages=[{"role": "user", "content": query}])
         response = completion.choices[0].message.content
-
-        # completion = openai.Completion.create(model="ada", prompt="Hello world")
-        # response = completion.choices[0].text
-
         print("GPT-3 response: " + response)
         return response
 
-    if request.is_json:
-        print("prompt received")
-        data = request.get_json()
-        prompt = data['prompt']
-        print(prompt)
-
-        result = openai_completion(prompt)
-
-        return jsonify({'response': result})
-    else:
-        return jsonify({'error': 'Invalid request'})
+    result = openai_completion(prompt)
+    return jsonify({'response': result})
