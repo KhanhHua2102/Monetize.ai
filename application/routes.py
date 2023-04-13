3d1c4ccd8a264ca37359bfaab501dcd65d0e7721
Quang Khanh (Henry) Hua <Quang.khanh2102@gmail.com>
2023-04-13 02:05:17 +0000

    change to flask app file structure

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,8 @@
 from application import app
-from flask import Flask, jsonify, request, render_template
+from flask import render_template
 
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     return render_template('index.html')
+@app.route("/")
+@app.route("/index")
+def index():
+    return "<h1>hello world</h1>"
 
