caf9a54156a3d5e72fa62d0a56c74e2086701eb1
tinsein <ivannoxic632@gmail.com>
2023-04-19 11:36:58 +0800

    changes made after the merge

--- application/routes.py
+++ application/routes.py
@@ -1,39 +1,37 @@
 from application import app
 from flask import render_template
 
-@app.route('/')
-@app.route('/AboutUs')
-def AboutUs():
-    return render_template('AboutUs.html')
 
-@app.route('/HelpCenter')
-def HelpCenter():
-    return render_template('HelpCenter.html')
-
-@app.route('/History')
-def History():
-    return render_template('History.html')
 @app.route("/")
 @app.route("/index")
 @app.route("/home")
 def index():
     return render_template('chat-screen.html')
 
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('AboutUs.html')
+
+
+@app.route('/helpcenter')
+def helpcenter():
+    return render_template('HelpCenter.html')
+
+
+@app.route('/history')
+def history():
+    return render_template('History.html')
+
+
 @app.route("/portfolio")
 def portfolio():
     return render_template('portfolio.html')
 
-# @app.route("/history")
-# def history():
-#     return render_template('history.html')
 
 # @app.route("/settings")
 # def settings():
 #     return render_template('settings.html')
-
-# @app.route("/aboutUs")
-# def aboutUs():
-#     return render_template('aboutUs.html')
 
 # @app.route("/logOut")
 # def logOut():
