90d6ff697825bdd4bd0a0dc2309633157d40f512
Quang Khanh Hua (Henry) <22928469@student.uwa.edu.au>
2023-04-21 13:23:24 +0800

    chatbot now have short term memory with context

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ openai.api_key = config.OPENAI_API_KEY
 
 CORS(app)
 
+context_data = 'You are a friendly financial chatbot. The user will ask you questions, and you will provide polite responses.\n\n'
 
 @app.route('/generate', methods=['POST'])
 def generate():
@@ -16,13 +17,18 @@ def generate():
     prompt = data['prompt']
     print(prompt)
 
+    global context_data
+    context_data += 'Q: ' + prompt + '\nA: '
+
     def openai_completion(query):
         print("Starting GPT-3\n")
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo", messages=[{"role": "user", "content": query}])
         response = completion.choices[0].message.content
-        print("GPT-3 response: " + response)
+        global context_data
+        context_data += response + '\n\n'
         return response
 
-    result = openai_completion(prompt)
+    result = openai_completion(context_data)
+    print(context_data)
     return jsonify({'response': result})
