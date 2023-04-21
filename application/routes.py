fc6efc84bc3090594ad3816e365ce315561f6200
Quang Khanh Hua (Henry) <22928469@student.uwa.edu.au>
2023-04-21 12:46:48 +0800

    fix menu for some pages

--- application/routes.py
+++ application/routes.py
@@ -5,32 +5,27 @@ from flask import render_template
 @app.route("/index")
 @app.route("/home")
 def index():
-    return render_template('index.html', index=True)
+    return render_template('index.html')
 
-
-@app.route('/aboutUs')
-def aboutUs():
-    return render_template('about-us.html')
-
-
-@app.route('/helpCenter')
-def helpCenter():
-    return render_template('help-center.html')
-
+@app.route("/portfolio")
+def portfolio():
+    return render_template('portfolio.html', menuCss=True)
 
 @app.route('/history')
 def history():
     return render_template('history.html')
 
-
-@app.route("/portfolio")
-def portfolio():
-    return render_template('portfolio.html')
-
-
 # @app.route("/settings")
 # def settings():
 #     return render_template('settings.html')
+
+@app.route('/aboutUs')
+def aboutUs():
+    return render_template('about-us.html')
+
+@app.route('/help')
+def help():
+    return render_template('help.html')
 
 # @app.route("/logOut")
 # def logOut():
