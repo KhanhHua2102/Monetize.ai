c5fe2382a9d35480c71935fbd847d09c1f7506ad
tinsein <ivannoxic632@gmail.com>
2023-04-24 23:57:45 +0800

    putting all of the database and yfinance together

--- app.py
+++ app.py
@@ -5,23 +5,16 @@ from flask_cors import CORS
 import config
 from datetime import datetime
 import openai
-<<<<<<< HEAD
-import os
-import yfinance as yf
-from dateutil import parser
-
-=======
 import yfinance as yf
 from datetime import datetime
 from dateutil import parser
 import re
->>>>>>> origin/Long
+import os
 
 openai.api_key = config.OPENAI_API_KEY
 
 CORS(app)
 
-<<<<<<< HEAD
 context_data = 'You are a friendly financial chatbot. The user will ask you questions, and you will provide polite responses.\n\n'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path+"/../", 'test.db')
 
@@ -46,15 +39,12 @@ def get_json_object(key,table,column_list=None):
             except AttributeError:
                 return {'error':f"{column} is not a valid column name"}
 
-
     except AttributeError:
         return {'error': f"{table} is not a valid table name."}
     return jsonify(data)
     # return jsonify({'response',data})
 
 # add new user to user table in database
-
-
 def addUser(userId, email, password):
     get_json_object()
 
@@ -66,8 +56,6 @@ def updateStock(user, ticker, vol, price, date):
 
 def addMessages():
     pass
-=======
-from dateutil import parser
 
 def parse_prompt(prompt):
     try:
@@ -104,7 +92,30 @@ def parse_prompt(prompt):
         return None, None, None, None, "Invalid prompt format: please enter the prompt in the format 'num_shares ticker start_date end_date'."
 
 
+def addingstock(thisprompt):
+    stock_context = "Based on a user's input, you have to determine if they want to add stocks to their portfolio. Return 'False' if they don't want to add stocks if their input is irrelevant to stocks. You should also return 'False' if their information does include the stock name, date added, the quantity of stock and bought price. Otherwise please return 'True'. For all query strictly return either True or False only and nothing else. The user input is:"
+    query = stock_context+'"'+thisprompt+'"'
+    response = openai_completion(query)
+    if response == "True":
+        return True
+    return False
 
+def commit_database_portfolio(formatted_prompt):
+    quantity,stock_type,date_added,price_bought,current_price, return_percent,return_amount,total = formatted_prompt.split(" ")
+    date_added_format = datetime.strptime(date_added,'%d/%m/20%y').date()
+    new_portfolio = models.portfolio1(stock_type=stock_type,date_added=date_added_format,quantity=quantity,price_bought=float(price_bought),current_price=float(current_price),return_percent=float(return_percent),return_amount=float(return_amount),total=float(total))
+    models.db.session.add(new_portfolio)
+    models.db.session.commit()
+    models.db.session.close()
+
+    
+def openai_completion(query):
+        print("Starting GPT-3\n")
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=[{"role": "user", "content": query}])
+        response = completion.choices[0].message.content
+        return response
+        
 def generate_response(num_shares, ticker, start_date, end_date):
     try:
         # Retrieve stock info and extract company name
@@ -128,93 +139,52 @@ def generate_response(num_shares, ticker, start_date, end_date):
 
     return response
 
-context_data = 'You are a friendly financial chatbot. The user will ask you questions, and you will provide polite responses.\n\n'
->>>>>>> origin/Long
 
-@app.route('/generate', methods=['POST'])
-def generate():
-    print("prompt received")
-    data = request.get_json()
-    prompt = data['prompt']
-    print(prompt)
-
+def generate_profit(input):
     global context_data
-<<<<<<< HEAD
-    context_data += 'Q: ' + prompt + '\nA: '
-
-=======
-    
-    context_data += 'Q: ' + prompt + '\nA: '
-    
->>>>>>> origin/Long
-    def openai_completion(query):
-        print("Starting GPT-3\n")
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=[{"role": "user", "content": query}])
+    response_values = input.split()
+    if len(input) == 4 and response_values[0].isdigit() and all(isinstance(val, str) for val in response_values[1:]):
+        num_shares, ticker, start_date, end_date, error_msg = parse_prompt(response)
+        if error_msg:
+            response = error_msg
+        else:
+            response = generate_response(num_shares, ticker, start_date, end_date)
+            prompt = context_data + 'Q: Using this information to give users a message:' + response + '\n'
+            response = openai_completion(prompt)
         
-        response = completion.choices[0].message.content
-        global context_data
-        context_data += response + '\n\n'
-<<<<<<< HEAD
-        return response
-    
-    def addingstock(thisprompt):
-        stock_context = "Based on a user's input, you have to determine if they want to add stocks to their portfolio. Return 'False' if they don't want to add stocks if their input is irrelevant to stocks. You should also return 'False' if their information does include the stock name, date added, the quantity of stock and bought price. Otherwise please return 'True'. For all query strictly return either True or False only and nothing else. The user input is:"
-        query = stock_context+'"'+thisprompt+'"'
-        response = openai_completion(query)
-        if response == "True":
-            return True
-        return False
+    return response
 
-    def commit_database_portfolio(formatted_prompt):
-        quantity,stock_type,date_added,price_bought,current_price, return_percent,return_amount,total = formatted_prompt.split(" ")
-        date_added_format = datetime.strptime(date_added,'%d/%m/20%y').date()
-        new_portfolio = models.portfolio1(stock_type=stock_type,date_added=date_added_format,quantity=quantity,price_bought=float(price_bought),current_price=float(current_price),return_percent=float(return_percent),return_amount=float(return_amount),total=float(total))
-        models.db.session.add(new_portfolio)
-        models.db.session.commit()
-        models.db.session.close()
-
-
-    result = openai_completion(prompt)
-
-    return jsonify({'response': result})
-
-with app.app_context():
-    models.db.create_all()
-
-=======
-        response_values = response.split()
-        if len(response_values) == 4 and response_values[0].isdigit() and all(isinstance(val, str) for val in response_values[1:]):
-         num_shares, ticker, start_date, end_date, error_msg = parse_prompt(response)
-         if error_msg:
-        
-          response = error_msg
-         else:
-          response = generate_response(num_shares, ticker, start_date, end_date)
-          context_data = 'Using this information to give users a message:'+ response
-          response = openai_completion(context_data)
-
-        return response
-    
-    
-    def contains_stock_info(context_data):
+def contains_stock_info(context_data):
      # check if context_data contains information about stocks, shares, and dates
      regex = r"\b(stocks|shares|tickers)\b.*\b(\d{1,2}(st|nd|rd|th)?\s(of)?\s[A-Z][a-z]{2,8}|\d{1,2}[-/]\d{1,2}[-/]\d{2,4}|\d{2,4}[-/]\d{1,2}[-/]\d{1,2}|\d{1,2}\s[A-Z][a-z]{2,8}\s\d{2,4}|\d{2,4}\s[A-Z][a-z]{2,8}\s\d{1,2}|\b(today|yesterday|tomorrow)\b)\b"
      return bool(re.search(regex, context_data, re.IGNORECASE))
-      
 
-    if contains_stock_info(prompt):
-      
+@app.route('/generate', methods=['POST'])
+def generate():
+    global context_data
+    print("user message received\n")
+    data = request.get_json()
+    userMessage = data['prompt']
+    print(userMessage + '\n')
+    if contains_stock_info(userMessage):
     # perform a specific action for when stock information is present
-     print("Stock information detected in context_data. Performing specific action...")
-     context_data = 'Convert this data so that it is able to input into Yfinance, must not caculate profit: {number of Shares} {Ticker} {Start-Date} {End-Date} must not caculate profit, User message: '+prompt+'Response must follow formats of convertsion only no need any comments: No need any Punctuation, the dates must be converted to dd/mm/yyyy, default end date is the word today no need to assume.' '\n'
-     result = openai_completion(context_data)
+        print("Stock information detected in context_data. Performing specific action...\n")
+        context_data += 'Q: Convert this data so that it is able to input into Yfinance, must not caculate profit: {number of Shares} {Ticker} {Start-Date} {End-Date} must not caculate profit, User message: '+ userMessage +'Response must follow formats of convertsion only no need any comments: No need any Punctuation, the dates must be converted to dd/mm/yyyy, default end date is the word today no need to assume.\nA: '
+        result = generate_profit(userMessage)
+        context_data += result
     # insert your code here
     else:
-     result = openai_completion(context_data)
-     print("No stock information detected in context_data.")
+        context_data += 'Q: ' + userMessage + '\nA: '
+        result = openai_completion(context_data)
+        context_data += result
+        print("No stock information detected in context_data.\n")
     
+    print(result)
     return jsonify({'response': result})
+
+
+with app.app_context():
+    models.db.create_all()
 
 # string="Convert this data so that it is able to input into: 
 # {number of Shares} {Ticker} {Start-Date} {End-Date} User message: 
@@ -222,4 +192,3 @@ with app.app_context():
 # Noted no need of comment and only pure Convertsion and make sure space in between, 
 # which mean you dont need to comment just the convertsion itself. 
 # The date  must be in dd/mm/yyyy, default endate is today date, Ticker Symbols can be input to Yfinance"
->>>>>>> origin/Long
