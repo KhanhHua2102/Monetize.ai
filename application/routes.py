6336b197123ae16dbd5dffa50f3a8a1e3cc28fbe
Quang Khanh Hua (Henry) <22928469@student.uwa.edu.au>
2023-05-01 01:27:38 +0800

    changes from merge Long branch

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from application import app, models
-from flask import render_template, redirect, url_for, session, make_response,flash,get_flashed_messages
+from flask import render_template, redirect, url_for, session, make_response, flash, get_flashed_messages
 from datetime import timedelta
 
 from forms import LoginForm, SignupForm
@@ -14,33 +14,40 @@ app.config['SECRET_KEY'] = secrets_key
 @app.route("/index")
 @app.route("/home")
 def index():
-    if'email' in session:
+    if 'email' in session:
         return render_template('index.html')
     else:
         return redirect(url_for('login'))
 
+
 @app.route('/portfolio')
 def portfolio():
-    portfolio = models.portfolio.query.order_by(models.portfolio.date_added).all()
+    portfolio = models.portfolio.query.order_by(
+        models.portfolio.date_added).all()
     return render_template('portfolio.html', mobileCSS=True, **locals())
+
 
 @app.route('/history')
 def history():
     return render_template('history.html')
 
+
 @app.route('/settings')
 def settings():
-    email = session['Email']
+    email = session['email']
     user_data = sql.getUserData(email)
-    return render_template('settings.html',menuCss=True, user_data=user_data)
+    return render_template('settings.html', menuCss=True, user_data=user_data)
+
 
 @app.route('/help')
 def help():
     return render_template('help.html')
 
+
 @app.route('/about-us')
 def aboutUs():
     return render_template('about-us.html')
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -51,32 +58,31 @@ def login():
 
         user_data = sql.getUserData(email)
         if not user_data:
-         flash('User not found', 'error')
-         return redirect(url_for('login'))
+            flash('User not found', 'error')
+            return redirect(url_for('login'))
 
         if password != user_data.password:
-          flash('Incorrect Password', 'error')
-          return redirect(url_for('login'))
+            flash('Incorrect Password', 'error')
+            return redirect(url_for('login'))
 
-        session['Email'] = email
+        session['email'] = email
         session.permanent = True  # make the session cookie permanent
         app.permanent_session_lifetime = timedelta(days=1)
 
-        # Add the Email cookie
+        # Add the email cookie
         response = make_response(redirect(url_for('index')))
-        response.set_cookie('Email', email)
+        response.set_cookie('email', email)
 
         return response
 
     return render_template('login.html', form=form)
 
 
-
 @app.route("/logOut", methods=['GET', 'POST'])
 def logOut():
     session.pop('email', None)
     form = LoginForm()
-    response = make_response(render_template('login.html',form=form))
+    response = make_response(render_template('login.html', form=form))
     response.delete_cookie('email')
     return response
 
@@ -96,5 +102,3 @@ def signup():
             return render_template('signup.html', form=form, error_message=error_message)
         return redirect(url_for('login'))
     return render_template('signup.html', form=form)
-
-
