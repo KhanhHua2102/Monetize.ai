8836bde9ac72e0aa79254404b66f1d7b3aab1930
Long Nguyen <hoanglongnt01@gmail.com>
2023-05-20 23:17:20 +0800

    Update Documentation for Css and JS

--- application/static/js/menu.js
+++ application/static/js/menu.js
@@ -1,5 +1,13 @@
+/*
+  This is the menu.js file, which contains JavaScript code for handling the menu functionality.
+  It uses jQuery to interact with the DOM and handle events.
+  The code toggles the visibility of menu links when the menu icon is clicked.
+*/
+
 $(document).ready(function () {
+	// Event handler for menu icon click
 	$("#menu-icon").click(function () {
-		$(".menu a").toggle();
+	  $(".menu a").toggle();
 	});
-});
+  });
+  
--- application/static/js/portfolio.js
+++ application/static/js/portfolio.js
@@ -1,12 +1,19 @@
-$(document).ready(function () {	
-	$('.download-csv button').click(function () {
-		console.log("export csv button clicked");
-		exportCSV();
-	});
-});
+/*
+  This is the Portfolio.js file, which contains JavaScript code for handling portfolio-related functionality.
+  It uses jQuery and Chart.js to interact with the DOM and create charts.
+  The code includes functions for exporting portfolio data as a CSV file and creating a pie chart using Chart.js.
+*/
 
-// Define table data
-let portfolioData = {
+$(document).ready(function () {
+	// Event handler for export CSV button click
+	$('.download-csv button').click(function () {
+	  console.log("export csv button clicked");
+	  exportCSV();
+	});
+  });
+  
+  // Define table data for the portfolio
+  let portfolioData = {
 	stocks: [],
 	dates: [],
 	quantities: [],
@@ -15,12 +22,12 @@ let portfolioData = {
 	returnsPercent: [],
 	returnsDollar: [],
 	totals: []
-}
-
-// Get table data for portfolioData
-let table = $("#portfolio-table");
-let rows = table.find("tr");
-for (let i = 1; i < rows.length; i++) {
+  };
+  
+  // Get table data for portfolioData
+  let table = $("#portfolio-table");
+  let rows = table.find("tr");
+  for (let i = 1; i < rows.length; i++) {
 	let cells = $(rows[i]).find("td");
 	portfolioData.stocks.push(cells[0].innerHTML);
 	portfolioData.dates.push(cells[1].innerHTML);
@@ -30,10 +37,10 @@ for (let i = 1; i < rows.length; i++) {
 	portfolioData.returnsPercent.push(cells[5].innerHTML);
 	portfolioData.returnsDollar.push(cells[6].innerHTML);
 	portfolioData.totals.push(cells[7].innerHTML);
-}
-
-// Default colors for piechart
-let defaultColors = [
+  }
+  
+  // Default colors for the pie chart
+  let defaultColors = [
 	"rgb(54, 162, 235)",
 	"rgb(75, 192, 192)",
 	"rgb(255, 205, 86)",
@@ -44,67 +51,68 @@ let defaultColors = [
 	"rgb(0, 128, 0)",
 	"rgb(255, 215, 0)",
 	"rgb(128, 0, 0)",
-];
-
-// Get the canvas element
-let ctx = $("#portfolio-chart")[0].getContext("2d");
-
-// Remove the $ from return ($) and totals
-let totals = [];
-for (let i = 0; i < portfolioData.stocks.length; i++) {
-	totals[i] = portfolioData.totals[i].substring(1,);
-}
-
-// Create the pie chart 
-let myChart = new Chart(ctx, {
+  ];
+  
+  // Get the canvas element for the pie chart
+  let ctx = $("#portfolio-chart")[0].getContext("2d");
+  
+  // Remove the $ symbol from return ($) and totals
+  let totals = [];
+  for (let i = 0; i < portfolioData.stocks.length; i++) {
+	totals[i] = portfolioData.totals[i].substring(1);
+  }
+  
+  // Create the pie chart using Chart.js
+  let myChart = new Chart(ctx, {
 	type: "pie",
 	data: {
-		labels: portfolioData.stocks,
-		datasets: [
-			{
-				label: " totals",
-				data: totals,
-				backgroundColor: defaultColors,
-			},
-		],
+	  labels: portfolioData.stocks,
+	  datasets: [
+		{
+		  label: "totals",
+		  data: totals,
+		  backgroundColor: defaultColors,
+		},
+	  ],
 	},
-});
-
-// Export CSV file when button is clicked
-function exportCSV() {
-    let csv = convertToCSV(portfolioData);
-    let blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+  });
+  
+  // Export portfolio data as a CSV file when the button is clicked
+  function exportCSV() {
+	let csv = convertToCSV(portfolioData);
+	let blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
 	let link = $('<a>').attr({
-		href: URL.createObjectURL(blob),
-		download: "portfolio.csv",
-		style: "display: none",
+	  href: URL.createObjectURL(blob),
+	  download: "portfolio.csv",
+	  style: "display: none",
 	});
 	$().append(link);
 	link[0].click();
 	$().remove(link);
-}
-
-// Convert portfolioData to CSV
-function convertToCSV(data) {
-    let csv = "Stock,Date,Quantity,Bought Price,Current Price,Returns Percent,Returns Dollar,Totals\r";
-    for (let i = 0; i < data.stocks.length; i++) {
-        csv +=
-					data.stocks[i] +
-					"," +
-					data.dates[i] +
-					"," +
-					data.quantities[i] +
-					"," +
-					data.bougthPrice[i] +
-					"," +
-					data.currentPrice[i] +
-					"," +
-					data.returnsPercent[i] +
-					"," +
-					data.returnsDollar[i] +
-					"," +
-					data.totals[i] +
-					"\r";
-    }
-    return csv;
-}
+  }
+  
+  // Convert portfolioData to CSV format
+  function convertToCSV(data) {
+	let csv = "Stock,Date,Quantity,Bought Price,Current Price,Returns Percent,Returns Dollar,Totals\r";
+	for (let i = 0; i < data.stocks.length; i++) {
+	  csv +=
+		data.stocks[i] +
+		"," +
+		data.dates[i] +
+		"," +
+		data.quantities[i] +
+		"," +
+		data.bougthPrice[i] +
+		"," +
+		data.currentPrice[i] +
+		"," +
+		data.returnsPercent[i] +
+		"," +
+		data.returnsDollar[i] +
+		"," +
+		data.totals[i] +
+		"\r";
+	}
+	return csv;
+  }
+  
