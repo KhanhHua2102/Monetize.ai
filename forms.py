dd08f4a73ad151e7cd58a1facd0f25a978cb1da4
tinsein <ivannoxic632@gmail.com>
2023-06-13 19:29:25 +0800

    Back to original

--- forms.py
+++ forms.py
@@ -1,17 +1,17 @@
-# from flask_wtf import FlaskForm
-# from wtforms import PasswordField, StringField, SubmitField
-# from wtforms.validators import DataRequired, Email, EqualTo, Length
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 
-# class LoginForm(FlaskForm):
-#     email = StringField('Email', validators=[DataRequired(), Email()])
-#     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
-#     submit = SubmitField('Login')
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField('Login')
 
-# class SignupForm(FlaskForm):
-#     name = StringField('Full Name', validators=[DataRequired()])
-#     email = StringField('Email Address', validators=[DataRequired(), Email()])
-#     phone = StringField('Phone Number', validators=[DataRequired()])
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+class SignupForm(FlaskForm):
+    name = StringField('Full Name', validators=[DataRequired()])
+    email = StringField('Email Address', validators=[DataRequired(), Email()])
+    phone = StringField('Phone Number', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     
