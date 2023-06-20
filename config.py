29523426a4c8e33434a3dcfa2cc1554488381036
tinsein <ivannoxic632@gmail.com>
2023-06-20 21:05:01 +0800

    hosting branch to host

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from boto.s3.connection import S3Connection
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
 FLASK_APP = 'app.py'
@@ -7,9 +8,11 @@ DEBUG = True
 class Config(object):
 
     # Enter your Open API Key here
-    OPENAI_API_KEY = ''
+    S3 = S3Connection(os.environ['OPEN_AI_key'], os.environ['FINNHUB_API_KEY'])
+    OPENAI_API_KEY = S3.get_bucket('OPEN_AI_key')
+    FINNHUB_API_KEY = S3.get_bucket('FINNHUB_API_KEY')
 
-    FINNHUB_API_KEY = os.environ["FINNHUB_API_KEY"]
+    # FINNHUB_API_KEY = 'ch4k2vpr01quc2n4rj5gch4k2vpr01quc2n4rj60'
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(base_dir, 'app.db')
@@ -22,4 +25,3 @@ class ProductionConfig(Config):
 class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(base_dir, 'test.db')
-    
